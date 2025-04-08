44363355fad1d86ab08e2dd74e53b2dba6bbdb1c
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-04-08 22:44:41 +0300

    comment example dot format

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ print('hi {}. you are {} years old'.format(name, age))
 print('hi {1}. you are {0} years old'.format(name, age))
 
 #used by dot format
-print('hi {new_name}. you are {age} years old'.format(new_name= 'hamda', age= 5))
+# print('hi {new_name}. you are {age} years old'.format(new_name= 'hamda', age= 5))
 
 #used by short format
 # print(f'hi {name}. you are {age} years old')
