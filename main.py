798dcb9752d4d3ba2b2a63413bf7607bfdfb4f7d
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-04-08 23:22:35 +0300

    add comment example used valid number

--- main.py
+++ main.py
@@ -249,9 +249,15 @@
 # print(selfish[0:8:2])
 
 #WHAT HAPPENT WHEN IT USE VALID NUMBER
-selfish = '01234567'
+# selfish = '01234567'
 print(selfish[1:])
+# another valid number
+print(selfish[:5])
 
+# using two semicolon
+print(selfish[::1])
+
+print(selfish[:-0])
 
 
 
